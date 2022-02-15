8ffe13b3bb096ac5e417878ad63a114966ac62b0
isanjit3 <isanjit3@gmail.com>
2022-02-15 15:56:14 -0500

    added variable delta

--- server.js
+++ server.js
@@ -1,28 +1,49 @@
 const express = require('express');
 const app = express();
 const http = require('http');
+const { hrtime } = require('process');
 const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
 
-app.use('/', express.static('web'))
 
 io.on('connection', (socket) => {
     console.log('a user connected');
 
-
+    socket.on("inputs", function(data){
+        
+    })
 
 });
 
+app.use('/', express.static('web'))
+
+
+const hrtimeMs = function() {
+    let time = process.hrtime()
+    return time[0] * 1000 + time[1] / 1000000
+}
+
 const TICK_DELTA = 1/60
+let tick = 0
+let previous = hrtimeMs()
+let tickLengthMs = 1000 / TICK_DELTA
+
 function gameLoop(delta){
+    setTimeout(gameLoop, tickLengthMs)
+    let now = hrtimeMs();
+    let delta = (now - previous) / 1000
+    console.log('delta', delta)
+    // game logic would go here
+
     // take into account player inputs
 
     // calculate physics/collisions
 
     // send the new game state to the players
+    previous = now
+    tick++
 
-    setTimeout(gameLoop, TICK_DELTA)
 }
 
 server.listen(3000, () => {
@@ -31,5 +52,6 @@ server.listen(3000, () => {
 
 
 gameLoop(TICK_DELTA)
+
 
 
--- web/index.js
+++ web/index.js
@@ -1,2 +1,9 @@
 var socket = io();
 
+// setup PIXI.js - game scene.
+
+// start the loop
+
+// check for player inputs
+inputs = {"left": false}
+socket.emit("inputs", inputs)
